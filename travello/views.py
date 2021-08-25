5d5539e959b507e032d55ffb2eda4e8883b39588
shirinkfathima <shirinkfathima@gmail.com>
2021-08-25 18:10:50 +0530

    objcts

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import Destination 
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html",{'price':700})
+    dest1=Destination()
+    dest1.name='Mumbai'
+    dest1.desc='The City That never Sleep'
+    dest1.price='800'
+
+   
+    dest2=Destination()
+    dest2.name='Kerala'
+    dest2.desc='The Gods own Country!'
+    dest2.price='1000'
+    return render(request,"index.html",{'dest1':dest1,'dest2':dest2})
